15a45447fc496d5af56b8e88e678347647cfbee3
Gary Brown <96browng@users.noreply.github.com>
2018-04-07 14:13:49 +0100

    initial

---
+++ src/main/java/uk/nhs/nhsbsa/example/sampleapi/SampleApiApplication.java
@@ -0,0 +1,12 @@
+package uk.nhs.nhsbsa.example.sampleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApiApplication.class, args);
+	}
+}
